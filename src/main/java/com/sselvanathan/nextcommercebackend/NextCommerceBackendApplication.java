e0bb993a4a1c9fa5c1bd35876a47d92220dd0b4c
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-10-07 11:45:26 +0200

    init

---
+++ src/main/java/com/sselvanathan/nextcommercebackend/NextCommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sselvanathan.nextcommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextCommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextCommerceBackendApplication.class, args);
+    }
+
+}
