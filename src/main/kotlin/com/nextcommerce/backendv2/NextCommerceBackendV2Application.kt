b3d8d04c7f43085de5ce7fbb33e8e994bf49cef9
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-12-22 16:48:49 +0100

    init

---
+++ src/main/kotlin/com/nextcommerce/backendv2/NextCommerceBackendV2Application.kt
@@ -0,0 +1,11 @@
+package com.nextcommerce.backendv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextCommerceBackendV2Application
+
+fun main(args: Array<String>) {
+    runApplication<NextCommerceBackendV2Application>(*args)
+}
